8ab2e65adfacbfe9defab039d4b777915ed29147
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-07-24 18:40:09 -0500

    more reading, more learning, each challenge opens more doors. Having issues with Map bidirectional prototype object, so going to create my own. Added link to relevant SO post. Lazy Saturday

--- CodeCracker/CodeCracker.js
+++ CodeCracker/CodeCracker.js
@@ -1,6 +1,9 @@
 // https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
 // https://startfunction.com/bidirectional-map-javascript/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+// https://stackoverflow.com/questions/10699492/bi-directional-map-in-java
+
+
 const alphabet = Array.from(Array(26))
   .map((e, i) => i + 65)
   .map(x => String.fromCharCode(x))
