1567650ce2809ced2dcb28f0860f36831ad5afa0
DarthNull <servantofchristvigil@mail.com>
2021-12-04 03:28:25 -0500

    *

--- CodeCracker/CodeCracker.js
+++ CodeCracker/CodeCracker.js
@@ -1,13 +1,17 @@
 
 class BiCrypt {
   constructor() {
-    this.plainTxt = {}, // ciphers keys, alpha chars vals
-    this.cipherTxt = {} // alpha chars keys, ciphers vals
+    this.alpha = {}, // ciphers keys, alpha chars vals
+    this.cipher = {} // alpha chars keys, ciphers vals
   }
 
-  setEncryptionScheme (alpha, cipher) { // arrs
+  setEncryptionScheme (alphaTxt, cipherTxt) { // 2 arrs of same len
     // alpha => ciper chars keys, alpha chars values
     // ciper => alpha chars keys, ciper chars vals
+    for (let i = 0; i < alphaTxt.length; i++) {
+      this.alpha[cipherTxt[i]] = alphaTxt[i]
+      this.cipher[alphaTxt[i]] = cipherTxt[i]
+    }
   }
 
   encrypt () {
@@ -18,4 +22,48 @@ class BiCrypt {
   // convert ciper txt to alpha txt
   }
 
+  displaySelf() {
+    console.log("this.alpha: ", this.alpha)
+    console.log("this.cipher: ", this.cipher)
+  }
+
 }
+
+const alphabet = Array.from(Array(26))
+  .map((e, i) => i + 65)
+  .map(x => String.fromCharCode(x))
+
+  const cipherText = [
+    '!',
+    ')',
+    '"',
+    '(',
+    '£',
+    '*',
+    '%',
+    '&',
+    '>',
+    '<',
+    '@',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o'
+  ]
+
+  const enDeCrypt = new BiCrypt()
+
+  enDeCrypt.setEncryptionScheme(alphabet, cipherText)
+
+  enDeCrypt.displaySelf()
